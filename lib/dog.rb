ce1469d5f06a86fac60b279135aee38cf79e87a2
Rwanda S <rwanda.ace@gmail.com>
2020-03-17 13:07:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  
+  def name=(doggie_name)
+    @name = doggie_name
+  end
+  def name
+    @name
+  end
+  def breed=(doggie_breed)
+    @breed = doggie_breed
+  end
+  def breed
+    @breed
+  end
+  
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(my_name)
+    @name = my_name
+  end
+  def name
+    @name
+  end
+  def job=(my_job)
+    @job = my_job
+  end
+  def job
+    @job
+  end
+end
